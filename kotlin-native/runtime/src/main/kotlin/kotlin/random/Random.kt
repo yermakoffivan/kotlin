c401141379bae96ccd7d7cabafc515e38cb515e6
Nikolai Kasyanov <nikolai.kasyanov@jetbrains.com>
2026-09-18 19:35:13 +0200

    [K/N][stdlib] Make default Random thread safe
    The atomic code (that wasn't providing thread safety anyway) has been
    removed, and `NativeRandom` instances are now thread local.
    
    ^KT-56363 Fixed

--- kotlin-native/runtime/src/main/kotlin/kotlin/random/Random.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/random/Random.kt
@@ -3,43 +3,41 @@
  * that can be found in the LICENSE file.
  */
 
-@file:OptIn(ExperimentalAtomicApi::class)
-
 package kotlin.random
 
-import kotlin.concurrent.atomics.AtomicLong
-import kotlin.concurrent.atomics.ExperimentalAtomicApi
-import kotlin.system.getTimeNanos
+import kotlin.native.concurrent.ThreadLocal
 
-/**
- * The default implementation of pseudo-random generator using the linear congruential generator.
- */
-private object NativeRandom : Random() {
-    private const val MULTIPLIER = 0x5deece66dL
-    @Suppress("DEPRECATION_ERROR")
-    private val _seed = AtomicLong(mult(getTimeNanos()))
-
-    /**
-     * Random generator seed value.
-     */
-    private val seed: Long
-        get() = _seed.load()
-
-    private fun mult(value: Long) = (value xor MULTIPLIER) and ((1L shl 48) - 1)
-
-    private fun update(seed: Long): Unit {
-        _seed.store(seed)
-    }
-
-    override fun nextBits(bitCount: Int): Int {
-        update((seed * MULTIPLIER + 0xbL) and ((1L shl 48) - 1))
-        return (seed ushr (48 - bitCount)).toInt()
-    }
-
-    override fun nextInt(): Int = nextBits(32)
-}
-
-internal actual val defaultRandom: Random = NativeRandom
+@ThreadLocal
+internal actual val defaultRandom: Random = NativeRandom()
 
 internal actual fun doubleFromParts(hi26: Int, low27: Int): Double =
         (hi26.toLong().shl(27) + low27) / (1L shl 53).toDouble()
+
+/**
+ * The default implementation of pseudo-random generator using the linear congruential generator.
+ * Even though it's not thread safe per se, it's exposed via [defaultRandom] annotated with [ThreadLocal],
+ * making the [Random.Default] thread safe on Native platforms.
+ */
+private class NativeRandom : Random() {
+    private var state = stateFromSeed(SeedAllocator.nextSeed())
+    private fun stateFromSeed(seed: Long) = (seed xor MULTIPLIER) and MASK
+
+    override fun nextBits(bitCount: Int): Int {
+        val nextState = (state * MULTIPLIER + INCREMENT) and MASK
+        state = nextState
+        return (nextState ushr (MODULUS - bitCount)).toInt()
+    }
+
+    /** Resets the state using the given seed for testing purposes */
+    fun overrideSeed(seed: Long) {
+        state = stateFromSeed(seed)
+    }
+
+    private val MULTIPLIER = 0x5deece66dL
+    private val INCREMENT = 0xbL
+    private val MODULUS = 48
+    private val MASK = (1L shl MODULUS) - 1
+}
+
+/** Resets the state of [NativeRandom] using the given seed for testing purposes */
+internal fun overrideNativeRandomSeed(seed: Long) = (defaultRandom as NativeRandom).overrideSeed(seed)
