a8ee6499a264e495bf231422d9d0d2d70cc2327b
Andrii Rublov <andrii.rublov@jetbrains.com>
2026-09-21 18:26:43 +0200

    [JS, Gradle] Hack for SwcEnvSpec to work before advancing KGP bootstrap
    ^KT-82969 In progress

--- repo/gradle-build-conventions/swc-configuration/src/main/kotlin/org/jetbrains/kotlin/build/swc/SwcExtension.kt
+++ repo/gradle-build-conventions/swc-configuration/src/main/kotlin/org/jetbrains/kotlin/build/swc/SwcExtension.kt
@@ -2,34 +2,35 @@
  * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+
 package org.jetbrains.kotlin.build.swc
 
-@file:OptIn(InternalKotlinGradlePluginApi::class)
-
-import SystemPropertyClasspathProvider
+import kotlinBuildProperties
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.extra
-import org.gradle.kotlin.dsl.newInstance
-import org.jetbrains.kotlin.gradle.targets.js.swc.SwcEnvSpec
-import org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi
 
 abstract class SwcExtension(
     private val project: Project,
-    private val swc: SwcEnvSpec,
+    // Actually `org.jetbrains.kotlin.gradle.targets.js.swc.SwcEnvSpec`, see `SwcBridge` for the reason it is untyped here
+    private val swc: Any,
 ) {
     val swcVersion: String
-        get() = kotlinBuildProperties.versionsProperty("swc").get()
+        get() = project.kotlinBuildProperties.versionsProperty("swc").get()
 
-    val swcExecutablePath: Provider<String> = swc.executable.also {
+    // val swcExecutablePath: Provider<String> = swc.executable.also {
+    // TODO: Remove a line below after bootstrap in favor of the above commented code
+    val swcExecutablePath: Provider<String> = SwcBridge.getExecutable(swc).also {
         project.extra["swc.path"] = it
     }
 
     fun Test.setupSwc() {
-        with(swc) {
-            dependsOn(project.swcSetupTaskProvider)
-        }
+//        with(swc) {
+//            dependsOn(project.swcSetupTaskProvider)
+//        }
+        // TODO: Remove after bootstrap in favor of the above commented code
+        dependsOn(SwcBridge.getSetupTaskProvider(project, swc))
 
         val swcExecutablePath = swcExecutablePath
 
--- repo/gradle-build-conventions/swc-configuration/src/main/kotlin/swc-configuration.gradle.kts
+++ repo/gradle-build-conventions/swc-configuration/src/main/kotlin/swc-configuration.gradle.kts
@@ -1,18 +1,28 @@
-@file:OptIn(InternalKotlinGradlePluginApi::class)
-
+import org.gradle.kotlin.dsl.create
+import org.jetbrains.kotlin.build.swc.SwcBridge
 import org.jetbrains.kotlin.build.swc.SwcExtension
-import org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.targets.js.swc.SwcEnvSpec
-import org.jetbrains.kotlin.gradle.targets.js.swc.SwcPlugin
 
-project.plugins.apply(SwcPlugin::class.java)
-val swcEnvSpec = project.the<SwcEnvSpec>()
+//import org.jetbrains.kotlin.gradle.targets.js.swc.SwcEnvSpec
+//import org.jetbrains.kotlin.gradle.targets.js.swc.SwcPlugin
 
+//project.plugins.apply(SwcPlugin::class.java)
+//val swcEnvSpec = project.the<SwcEnvSpec>()
+//
+//val swcKotlinBuild = extensions.create<SwcExtension>(
+//    "swcKotlinBuild",
+//    swcEnvSpec,
+//)
+//
+//with(swcKotlinBuild) {
+//    swcEnvSpec.version.set(swcVersion)
+//}
+
+// TODO: Remove everything below after KGP bootstrap, see org.jetbrains.kotlin.build.swc.SwcBridge doc
+//   and uncomment the above code back.
+
+val swcEnvSpec = SwcBridge.applySwcPlugin(project)
 val swcKotlinBuild = extensions.create<SwcExtension>(
     "swcKotlinBuild",
     swcEnvSpec,
 )
-
-with(swcKotlinBuild) {
-    swcEnvSpec.version.set(swcVersion)
-}
+SwcBridge.setVersion(swcEnvSpec, swcKotlinBuild.swcVersion)
