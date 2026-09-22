dcae4044f8c05597541505444a32c05f9a13860e
Cristian Garcia <cristian.garcia-marin@jetbrains.com>
2026-09-22 10:41:33 +0200

    [Build] Deprecation of Kotlin DSL delegated properties
    https://docs.gradle.org/9.7.1/userguide/upgrading_version_9.html#kotlin_dsl_delegated_properties

--- kotlin-native/performance/buildSrc/src/main/kotlin/benchmark/SwiftBenchmarkingPlugin.kt
+++ kotlin-native/performance/buildSrc/src/main/kotlin/benchmark/SwiftBenchmarkingPlugin.kt
@@ -27,9 +27,9 @@ open class SwiftBenchmarkExtension @Inject constructor(project: Project) : Bench
      */
     val packageDirectory: DirectoryProperty = project.objects.directoryProperty()
 
-    val generateSwiftPackage by project.tasks.registering(GenerateSwiftPackageTask::class)
-    val buildSwift by project.tasks.registering(SwiftBuildTask::class)
-    val konanRun by project.tasks.registering(RunKotlinNativeTask::class)
+    val generateSwiftPackage = project.tasks.register<GenerateSwiftPackageTask>("generateSwiftPackage")
+    val buildSwift = project.tasks.register<SwiftBuildTask>("buildSwift")
+    val konanRun = project.tasks.register<RunKotlinNativeTask>("konanRun")
 }
 
 /**
