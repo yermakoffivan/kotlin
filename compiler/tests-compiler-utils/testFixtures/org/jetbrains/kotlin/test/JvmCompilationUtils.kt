d098b1b3189019cc04e43ce33451fa9501e8bf17
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-09-22 15:51:17 +0300

    Properly construct `--patch-module` options for `javac` in tests
    See: `PhasedJvmDiagnosticLightTreeTestGenerated.testPseudoRawTypes`,
    which otherwise fails with:
    
    ```
    error: package exists in another module: java.base
    package java.util;
    ^
    /private/var/folders/5r/wwlpp6t51j3910jcq7298jx40000kt/T/analysis-testsProject_test_13609197263127965719/org.jetbrains.kotlin.test.runners.PhasedJvmDiagnosticLightTreeTestGenerated_Tests_GenericstestPseudoRawTypes/java-sources/main/test/Usage.java:16: error: cannot find symbol
        c.foo();
         ^
      symbol:   method foo()
      location: variable c of type java.util.Collection
    ```
    
    In modern Java, it's no longer possible to "overload" the stdlib classes
    like it was in 9 - we need an explicit option to enable that.
    
    Another nice test: `PhasedJvmDiagnosticPsiTestGenerated.testClassFromJdkInLibrary`.

--- compiler/tests-compiler-utils/testFixtures/org/jetbrains/kotlin/test/JvmCompilationUtils.kt
+++ compiler/tests-compiler-utils/testFixtures/org/jetbrains/kotlin/test/JvmCompilationUtils.kt
@@ -65,7 +65,7 @@ fun compileJavaFiles(files: Collection<File>, options: List<String?>, jdkHome: F
                 compilerOutputWriter,
                 fileManager,
                 diagnosticCollector,
-                options + listOf("-Djava.ext.dirs="),
+                options + listOf("-Djava.ext.dirs=") + extractPatchModuleOptions(files),
                 null,
                 javaFileObjectsFromFiles
             )
@@ -78,6 +78,15 @@ fun compileJavaFiles(files: Collection<File>, options: List<String?>, jdkHome: F
         }
     }
 }
+
+private val JAVA_SOURCE_SET_PATH_PATTERN = """(.*/java-sources/[^/]+/java/).*""".toRegex()
+
+private fun extractPatchModuleOptions(files: Collection<File>): List<String> =
+    files.mapNotNullTo(mutableSetOf()) {
+        JAVA_SOURCE_SET_PATH_PATTERN.matchEntire(it.path)?.groupValues?.getOrNull(1)
+    }.flatMap {
+        listOf("--patch-module", "java.base=$it")
+    }
 
 private fun compileJavaFilesExternally(files: Collection<File>, options: List<String?>, jdkHome: File): JavaCompilationResult {
     val command: MutableList<String?> = ArrayList()
